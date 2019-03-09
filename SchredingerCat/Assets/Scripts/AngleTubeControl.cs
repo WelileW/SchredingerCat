173e40b8bcb2a07e1d693351e73ff97eb346c903
eles <vilena1991@gmail.com>
2019-03-09 16:03:41 +0300

    add controls for tubes

---
+++ SchredingerCat/Assets/Scripts/AngleTubeControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleTubeControl : TubeControl
+{
+    void Awake()
+    {
+        _connected = new List<SideEnum>();
+
+        _connected.Add(SideEnum.Top);
+        _connected.Add(SideEnum.Left);
+    }
+}
---
+++ SchredingerCat/Assets/Scripts/SideEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum SideEnum
+{
+    Top = 0,
+    Right = 1,
+    Bottom = 2,
+    Left = 3
+}
