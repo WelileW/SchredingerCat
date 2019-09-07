3e86abf9fd633e20f02d7985865f79daf5cf72dd
welilew <vilena1991@gmail.com>
2019-09-08 00:59:34 +0300

    init new project

---
+++ Cat2/Cat/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControl : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        var selecting = gameObject.transform.GetChild(0).gameObject;
+        selecting.SetActive(true);
+    }
+
+    void OnMouseUp()
+    {
+        var selecting = gameObject.transform.GetChild(0).gameObject;
+        selecting.SetActive(false);
+    }
+}
